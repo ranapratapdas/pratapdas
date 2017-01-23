d1ec36a7ce17058787058892f30c0b09a8a1c7d7
Pratap Das <pratap@pratapdas.com>
2017-01-23 16:49:07 -0500

    Initial Creation

---
+++ pratapdas/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace pratapdas
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
